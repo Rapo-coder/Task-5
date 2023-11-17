9bd7474f9c29d0ad87e2ac070e443357f8fd52cd
Rapo-coder <sekhar.madapana@healthonus.io>
2023-11-17 18:36:11 +0530

    create edit and delete button and eidt functionality is done

--- homepage.js
+++ homepage.js
@@ -7,14 +7,17 @@ function closeDialog()
   document.getElementById('addDialog').style.display = 'none';
 }
 
+var isEditing=false;
+
+//add contact
 function addContact() 
 {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var mobile = document.getElementById('mobile').value;
-    var mobile = document.getElementById('landline').value;
-    var mobile = document.getElementById('website').value;
-    var mobile = document.getElementById('address').value;
+    var landline = document.getElementById('landline').value;
+    var website = document.getElementById('website').value;
+    var address = document.getElementById('address').value;
    
     var contact = 
     {
@@ -25,9 +28,21 @@ function addContact()
       website:website,
       address:address,
     };
-    saveContact(contact);
+    console.log(contact);
+    if(isEditing)
+    {
+      updateContact(contact);
+    }
+    else
+    {
+      saveContact(contact);
+    }
+    closeDialog();
+    isEditing = false;
   }
    
+  //save the contact
+
   function saveContact(contact) 
   {
     var existingContacts = JSON.parse(localStorage.getItem('contacts')) || [];
@@ -37,6 +52,8 @@ function addContact()
     closeDialog();
   }
    
+  //displaying contacts in homepage
+
   function displayContact() 
   {
       var existingContacts = JSON.parse(localStorage.getItem("contacts"))
@@ -45,30 +62,85 @@ function addContact()
       var contactElement = document.createElement('div');
       contactElement.className = 'contact';
       contactElement.innerHTML = `
-        Name: ${contact.name}<br>
-        Email: ${contact.email}<br>
-        Mobile: ${contact.mobile}<br><br>`;
+         ${contact.name}<br>
+         ${contact.email}<br>
+         ${contact.mobile}<br><br>`;
+         contactElement.onclick = function() 
+        {
+          showFullDetails(contact);
+        };  
+        contactElement.set
       contactList.appendChild(contactElement);
+      
     });
+
   }
   displayContact();
 
-   
+  //show full details of the contact
+
   function showFullDetails(contact) {
     var fullDetailsBox = document.getElementById('fullDetailsBox');
-    fullDetailsBox.innerHTML = `
-      Name: ${contact.name}<br>
-      Email: ${contact.email}<br>
-      <strong>Mobile:</strong> ${contact.mobile}<br>
-      landline: ${contact.landline}<br>
-      Website: ${contact.website}<br>
-      Address: ${contact.address}<br>;
-      `;
-  fullDetailsBox.style.display = 'block';
+      var detailsHTML = `
+          ${contact.name}<br>
+          Email: ${contact.email}<br>
+          Mobile: ${contact.mobile}<br>
+          Landline:${contact.landline}<br>
+          Website: ${contact.website}<br>
+          Addres: ${contact.address}<br>
+          `;
+          fullDetailsBox.innerHTML = detailsHTML;
+          
+          let editDeleteContainer = document.createElement("div");
+          editDeleteContainer.className = "editDeleteButtons";
+         
+          let editButton = document.createElement("button");
+          editButton.textContent = "Edit";
+         
+          let deleteButton = document.createElement("button");
+          deleteButton.textContent = "Delete";
+
+          editDeleteContainer.appendChild(editButton);
+          editDeleteContainer.appendChild(deleteButton);
+
+          fullDetailsBox.appendChild(editDeleteContainer);
+        
+          editButton.onclick = function()
+          {
+            openEditDailog(contact)
+          }
   }
 
-   
+  function closeDetailsBox() {
+    var fullDetailsBox = document.getElementById('fullDetailsBox');
+    fullDetailsBox.innerHTML = '';
+}
+ 
+//open edit dialog box
+function openEditDailog(contact)
+{
+  isEditing = true;
+  document.getElementById('name').value = contact.name;
+  document.getElementById('email').value = contact.email;
+  document.getElementById('mobile').value = contact.mobile;
+  document.getElementById('landline').value = contact.landline;
+  document.getElementById('website').value = contact.website;
+  document.getElementById('address').value = contact.address;
 
-   
-  
+  openDialog();
+}
+ 
+
+//Update the contact
+function updateContact(updatedContact) {
+  var existingContacts = JSON.parse(localStorage.getItem('contacts')) || [];
+  var index = existingContacts.findIndex(contact => contact.name === updatedContact.name);
+ 
+  if (index !== -1) {
+    existingContacts[index] = updatedContact;
+    localStorage.setItem('contacts', JSON.stringify(existingContacts));
+    alert('Contact updated successfully');
+  }
+}
+
 

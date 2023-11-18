0e386ae847c8be1f2f312ad1c2d5b088f414b24a
Rapo-coder <sekhar.madapana@healthonus.io>
2023-11-18 12:44:42 +0530

    changes done

--- homepage.js
+++ homepage.js
@@ -12,6 +12,7 @@ var isEditing=false;
 //add contact
 function addContact() 
 {
+
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var mobile = document.getElementById('mobile').value;
@@ -19,6 +20,14 @@ function addContact()
     var website = document.getElementById('website').value;
     var address = document.getElementById('address').value;
    
+    if (!isValidMobile(mobile)) {
+      alert("Please enter a valid mobile number.");
+      return;
+    }
+    if (!isValidEmail(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
     var contact = 
     {
       name: name,
@@ -41,6 +50,16 @@ function addContact()
     isEditing = false;
   }
    
+  function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+   
+  function isValidMobile(mobile) {
+    var mobileRegex = /^\d{10}$/;
+    return mobileRegex.test(mobile);
+  }
+
   //save the contact
 
   function saveContact(contact) 
@@ -82,33 +101,51 @@ function addContact()
   function showFullDetails(contact) {
     var fullDetailsBox = document.getElementById('fullDetailsBox');
       var detailsHTML = `
-          ${contact.name}<br>
-          Email: ${contact.email}<br>
+      <strong><h1>${contact.name}</h1></strong>
+          Email: ${contact.email}<br><br>
           Mobile: ${contact.mobile}<br>
-          Landline:${contact.landline}<br>
-          Website: ${contact.website}<br>
+          Landline:${contact.landline}<br><br>
+          Website: ${contact.website}<br><br>
+
           Addres: ${contact.address}<br>
           `;
           fullDetailsBox.innerHTML = detailsHTML;
+
+          
           
           let editDeleteContainer = document.createElement("div");
           editDeleteContainer.className = "editDeleteButtons";
-         
+       
           let editButton = document.createElement("button");
+          let editIcon = document.createElement('img')
+          editIcon.src ='assets/Edit-icon.png'
           editButton.textContent = "Edit";
-         
+          editButton.style.color = "#black";  
+          editButton.background= "white"
+       
           let deleteButton = document.createElement("button");
+          let deleteIcon = document.createElement('img')
+          deleteIcon.src ='assets/Edit-icon.png'
           deleteButton.textContent = "Delete";
-
+          deleteButton.style.color = "black";
+       
           editDeleteContainer.appendChild(editButton);
           editDeleteContainer.appendChild(deleteButton);
-
+       
           fullDetailsBox.appendChild(editDeleteContainer);
         
+          
           editButton.onclick = function()
           {
             openEditDailog(contact)
           }
+
+          deleteButton.onclick = function () {
+            if (confirm('Are you sure you want to delete this contact?')) {
+              deleteContact(contact);
+              closeDetailsBox();
+            }
+          };
   }
 
   function closeDetailsBox() {
@@ -136,11 +173,19 @@ function updateContact(updatedContact) {
   var existingContacts = JSON.parse(localStorage.getItem('contacts')) || [];
   var index = existingContacts.findIndex(contact => contact.name === updatedContact.name);
  
-  if (index !== -1) {
+  if (index != -1) {
     existingContacts[index] = updatedContact;
     localStorage.setItem('contacts', JSON.stringify(existingContacts));
     alert('Contact updated successfully');
   }
 }
 
+
+//Delete Contacts
+function deleteContact(contact) {
+  var existingContacts = JSON.parse(localStorage.getItem('contacts')) || [];
+  var contact = existingContacts.filter(existingContact => existingContact.name !== contact.name);
+  localStorage.setItem('contacts', JSON.stringify(contact));
+  alert('Contact deleted successfully');
+  }
 
